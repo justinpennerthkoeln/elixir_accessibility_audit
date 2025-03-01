6da3d4d03c25318271dcbcd6802e644fdaec3aae
justinpennerthkoeln <justin_dan.penner@smail.th-koeln.de>
2025-03-01 21:53:26 +0100

    fixing css

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -10,9 +10,9 @@ defmodule HelloWeb.PageController do
     if user_key != nil do
       user = Users.get_user_by_uuid(user_key)
       api_keys = ApiKeys.get_api_key_by_user_id(user.id)
-      render(conn, :dashboard, page_title: "Dashboard", layout: false, api_keys: api_keys, user_id: user.id, base_path: System.get_env("BASE_PATH"))
+      render(conn, :dashboard, page_title: "Dashboard", layout: false, api_keys: api_keys, user: user, base_path: System.get_env("BASE_PATH"))
     else
-      render(conn, :dashboard, page_title: "Dashboard", layout: false, api_keys: [], user_id: nil, base_path: System.get_env("BASE_PATH"))
+      render(conn, :dashboard, page_title: "Dashboard", layout: false, api_keys: [], user: nil, base_path: System.get_env("BASE_PATH"))
     end
 
   end
